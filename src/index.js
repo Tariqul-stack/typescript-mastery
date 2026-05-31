e924a8daed8dbdd0a1d9b0d2bff922b6629ec312
Tariqul911 <zxtariqul911@gmail.com>
2026-05-31 11:57:49 +0800

    chore: initialize typescript learning repository

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Tariqul";
+const age = 18;
+console.log(`My name is ${name}`);
+console.log(`My age is ${age}`);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const name: string = "Tariqul";
+
+const age: number = 18;
+
+console.log(`My name is ${name}`);
+console.log(`My age is ${age}`);
